39cb41d220658a5cf243d2d5ab7b777509fa6963
SpekalsG3 <karpick.kirill@gmail.com>
2019-09-16 22:24:48 +0300

    Added jump. Close to seem real

--- scripts/Platformer.js
+++ scripts/Platformer.js
@@ -1,32 +1,45 @@
 var layers = document.getElementsByClassName("background__layer");
 var character = document.getElementById("character");
 
-var backPos = 0;
-var speed = 8;
-var swrddrw = 3000;
+var backPos = 0;           // Background start position
+var speed = 8;             // Speed of moving, attack, background move etc.
+var swrdshte = 24000;      // Time factor of character staying in idle-2
 
 var keys = {
   left: false,
-  right: false
+  right: false,
+  jump: false
 }
 
 var condition = {
   _position: "idle",
   prevpos: "",
   requested: "",
-  status: 0
+  status: 0,
+  break: false
 }
 
 Object.defineProperty(condition, "position", {
-  set: function(value) {
+  set: function(pos) {
+    if (allConditions[pos].start)
+      allConditions[pos].start();
     this.requested = "";
     this.prevpos = this._position;
-    this._position = value;
+    this._position = pos;
   },
   get: function() {
     return this._position;
   }
 });
+
+var hero = {
+  ay: 15,
+  ax: 0,
+  vy: 0,
+  vx: 0,
+  y: 0,
+  x: 0
+}
 
 var allConditions = {
   "idle": {
@@ -36,6 +49,11 @@ var allConditions = {
     max: 5
   },
   "idle-2": {
+    start: function() {
+      setTimeout(function() {
+        setCondition("swrd-shte");
+      }, swrdshte / speed);
+    },
     max: 3
   },
   "swrd-drw": {
@@ -52,11 +70,16 @@ var allConditions = {
     }
   },
   "attack1": {
+    start: function() {
+      setTimeout(function() {
+        hitEnemy();
+      }, 1200 / speed * 2);
+    },
     max: 4,
     next: function() {
-      if (condition.requested) {
+      if (condition.requested)
         condition.position = condition.requested;
-      } else if (condition.prevpos == "run")
+      else if (condition.prevpos == "run")
         condition.position = "run";
       else 
         condition.position = "idle-2";
@@ -64,11 +87,16 @@ var allConditions = {
     unavoidable: true
   },
   "attack2": {
+    start: function() {
+      setTimeout(function() {
+        hitEnemy();
+      }, 1200 / speed * 3);
+    },
     max: 5,
     next: function() {
-      if (condition.requested) {
+      if (condition.requested)
         condition.position = condition.requested;
-      } else if (condition.prevpos == "run")
+      else if (condition.prevpos == "run")
         condition.position = "run";
       else 
         condition.position = "idle-2";
@@ -76,30 +104,72 @@ var allConditions = {
     unavoidable: true
   },
   "attack3": {
+    start: function() {
+      setTimeout(function() {
+        hitEnemy();
+      }, 1200 / speed * 2);
+    },
     max: 5,
     next: function() {
-      if (condition.requested) {
+      if (condition.requested)
         condition.position = condition.requested;
-      } else if (condition.prevpos == "run")
+      else if (condition.prevpos == "run")
         condition.position = "run";
       else 
         condition.position = "idle-2";
     },
     unavoidable: true
+  },
+  "jump": {
+    max: 3,
+    next: function() {
+      if (condition.requested)
+        this.requested = condition.requested;
+      if (condition.prevpos != "jump")
+        this.prevpos = condition.prevpos;
+      if (hero.vy > 0) {
+        condition.position = "jump";
+        condition.status = 3;
+      } else
+        condition.position = "fall";
+    },
+    requested: "",
+    prevpos: "",
+    back: false,
+    unavoidable: true
+  },
+  "fall": {
+    max: 1,
+    next: function(){
+      if (hero.y == 0) {
+        if (condition.requested)
+          condition.position = condition.requested;
+        else if (allConditions["jump"].requested) {
+          condition.position = allConditions["jump"].requested;
+          allConditions["jump"].requested = "";
+        }
+        else if (allConditions["jump"].prevpos) {
+          condition.position = allConditions["jump"].prevpos;
+          allConditions["jump"].prevpos = "";
+        } else
+          condition.position = "idle";
+      }
+    },
+    unavoidable: true
   }
 }
 
-function setCondition(pos) {
+function setCondition(pos, stat = 0) {
   if (allConditions[condition.position].unavoidable) {
     condition.requested = pos;
     return;
   }
-  condition.status = 0;
   condition.position = pos;
+  condition.status = stat;
 }
 
 function hitEnemy() {
-  console.log("OOOCH!");
+  console.log("OUUUCH!");
 }
 
 function attack() {
@@ -112,6 +182,14 @@ function attack() {
   }
 }
 
+function jump() {
+  setCondition("jump");
+  setTimeout(function(){
+    keys.jump = true;
+    hero.vy = 60;
+  }, 1200 / speed * 2)
+}
+
 document.addEventListener("keydown", function(e) {
   switch (e.keyCode) {
     case 37:
@@ -119,20 +197,20 @@ document.addEventListener("keydown", function(e) {
         break;
       character.style.transform = "rotateY(180deg)";
       setCondition("run");
-      keys = {
-        left: true,
-        right: false
-      }
+      keys.left = true;
+      keys.right = false;
       break;
     case 39:
       if (keys.right)
         break;
       character.style.transform = "rotateY(0deg)";
       setCondition("run");
-      keys = {
-        left: false,
-        right: true
-      }
+      keys.left = false;
+      keys.right = true;
+      break;
+    case 32:
+      if (!keys.jump)
+        jump();
       break;
     case 65:
       attack();
@@ -162,6 +240,19 @@ document.addEventListener("keyup", function(e) {
 });
 
 var backMove = setInterval(function() {
+  if (keys.jump) {
+    hero.vy -= hero.ay;
+    hero.y += hero.vy;
+
+    if (hero.y > 0) {
+      character.style.bottom = hero.y + 28 + "px";
+    } else {
+      keys.jump = false;
+      hero.y = 0;
+      character.style.bottom = "28px";
+    }
+  }
+
   if (keys.left) {
     if (backPos == 0) {
       setCondition("idle");
@@ -177,16 +268,16 @@ var backMove = setInterval(function() {
       layers[i].style.left = (parseInt(layers[i].getAttribute("data-index")) + 1) * backPos + "px";
     }
   }
-}, 150);
+}, 1200 / speed);
 
 var conditionLoop = setInterval(function() {
+  if (condition.break)
+    return;
   if (condition.status >= allConditions[condition.position].max) {
     condition.status = 0;
-    if (allConditions[condition.position].next) {
+    if (allConditions[condition.position].next)
       allConditions[condition.position].next();
-    }
-  } else {
+  } else
     condition.status++;
-  }
   character.setAttribute("src", "character/adventurer-" + condition.position + "-0" + condition.status + ".png");
 }, 1200 / speed);
